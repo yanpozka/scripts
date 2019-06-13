b69aa1e8594df3a613c428813517585cc1b73068
Yandry Pozo <yanpozka@users.noreply.github.com>
2019-06-13 12:43:15 -0600

    Update update-vim.sh

--- update-vim.sh
+++ update-vim.sh
@@ -23,6 +23,6 @@ done
 cd vim-go; git fetch; git checkout tags/v1.20
 
 # 256 colors
-tmpcolors_dir=/tmp/vim-colors-256
-git clone https://github.com/JarrodCTaylor/vim-256-color-schemes.git $tmpcolorsdir
-cp $tmpcolorsdir/colors/*.vim ~/.vim/colors
+mkdir -p /tmp/vim-colors-256
+git clone https://github.com/JarrodCTaylor/vim-256-color-schemes.git /tmp/vim-colors-256
+cp /tmp/vim-colors-256/colors/*.vim ~/.vim/colors
